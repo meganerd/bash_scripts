87dd99daebcf72ac84148cce5b2110056e35da74
gustin@meganerd.ca <gustin@meganerd.ca>
2014-03-31 15:15:39 -0600

    Added reboot_check.sh script.  This script checks Debian style systems to see if there is a pending reboot (perhaps due to an apt upgrade), and then reboots the system in the configured interval (in minutes).

---
+++ sys/reboot_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script checks a Debian like system to see if there is a pending reboot.
+# This script then reboots the system after the specified interval.  Use with | tee in 
+# crontab for logging.
+
+reboot_interval="5"
+current_date="`date`"
+
+if [ -f /var/run/reboot-required ]
+        then echo "Reboot required at $current_date, will reboot in $reboot_interval minutes." ;
+        /sbin/shutdown -r +$reboot_interval
+#       /sbin/reboot
+        else echo "No reboot required at this time ($current_date)."
+fi
+#exit 0
