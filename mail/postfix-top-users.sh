5663b6724864501299c03529b84b4a4273d65309
Gustin Johnson <gustin@meganerd.ca>
2014-06-19 12:51:39 -0600

    Added postfix-top-users.sh.

---
+++ mail/postfix-top-users.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NUsers=${1:-15}
+
+ShowUsage()
+{
+printf "This script requires you to provide the number of users you are interested in.  It will default to 15 if no parameter is provided"
+}
+
+TopUsers()
+{
+pflogsumm --iso_date_time  /var/log/mail.log | grep -A $NUsers "Senders by message count"
+}
+
+TopUsers
+
+
+
