faf924ddca889340d7deda8020cd9998213bd90a
gustin@meganerd.ca <gustin@meganerd.ca>
2014-09-08 09:59:28 -0600

    Fixed syntax error in flags since there were slight differences between apt-get and aptitude.

--- apt/update-apt.sh
+++ apt/update-apt.sh
@@ -2,8 +2,8 @@
 EMAIL=changeme@domain.tld
 PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin
 
-aptitude update -qq
-aptitude dist-upgrade -qq -d -y --force-yes
+aptitude update -q
+aptitude dist-upgrade -q -d -y --force-yes
 
 # On this system I am not using the -s flag which only simulates 
 # an install.  In this case I want to automatically install updates.  To simulate 
