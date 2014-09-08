9347c2ea13f6126369843a36943e9fde686df689
gustin@meganerd.ca <gustin@meganerd.ca>
2014-09-08 09:53:47 -0600

    Using aptitude instead of apt-get in update-apt.sh for better depndancy tracking.

--- apt/update-apt.sh
+++ apt/update-apt.sh
@@ -2,11 +2,11 @@
 EMAIL=changeme@domain.tld
 PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin
 
-apt-get update -qq
-apt-get dist-upgrade -qq -d -y --force-yes
+aptitude update -qq
+aptitude dist-upgrade -qq -d -y --force-yes
 
 # On this system I am not using the -s flag which only simulates 
 # an install.  In this case I want to automatically install updates.  To simulate 
 # add the -s flag to apt-get.
 
-apt-get dist-upgrade -u -y
+aptitude safe-upgrade -u -y
