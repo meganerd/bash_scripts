6655b5378f0585b6d1c6d407ff0165cb63e1b086
Gustin Johnson <gustin@meganerd.ca>
2014-09-16 00:16:50 -0600

    Update update-apt.sh
    Fixed minor syntax issues, specifically relating to aptitude parameters.

--- apt/update-apt.sh
+++ apt/update-apt.sh
@@ -3,10 +3,10 @@ EMAIL=changeme@domain.tld
 PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin
 
 aptitude update -q
-aptitude dist-upgrade -q -d -y --force-yes
+apt-get upgrade  -q -d 
 
 # On this system I am not using the -s flag which only simulates 
 # an install.  In this case I want to automatically install updates.  To simulate 
 # add the -s flag to apt-get.
 
-aptitude safe-upgrade -u -y
+aptitude safe-upgrade -y
