acefd0474147234b78116844f4232a7e12caa547
gustin@meganerd.ca <gustin@meganerd.ca>
2014-09-23 12:46:34 -0600

    Added ipv6-check.sh script.

---
+++ network/ipv6-check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Check for IPv6 connectivity, and restart the aiccu daemon
+# if this check fails
+
+# remote host used to test IPv6 connectivity
+remote_host=""
+
+# name and path for ssh key if using scp to test connectivity
+ssh_key=""
+
+# remote_check functions
+CheckNet_TCP ()
+{
+   touch ~/null.bin
+   scp -c -i $SSH_KEY ~/null.bin $HOST:$REMOTE_PATH/
+}
+
+CheckNet_ICMP ()
+{
+  ping6 -c 1 $HOST  >& /dev/null ; 
+}
+ # Check if available via ping or SCP, then transfer archive file if remote location is online.
+if CheckNet_ICMP ; then
+        echo "### Internet connection is alive and kicking, Nothing to be done. ###"
+        exit 0
+
+else
+  echo "### IPv6 connectivity test failed.  Restarting the aiccu daemon. ###"
+  service aiccu restart
+    exit 1
+fi
