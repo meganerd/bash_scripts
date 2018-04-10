a397cbb2514c9383400a3c56fc53db89b2b4f6a7
Gustin Johnson <Gustin.Johnson@fglsports.com>
2018-04-10 15:37:54 -0600

    Fixed a small syntax error in misc/check_cert.sh that impacted newer versions of GNU sed.

--- misc/check_cert.sh
+++ misc/check_cert.sh
@@ -6,7 +6,7 @@ port=$2
 echo "Common name and subject alternative names:"
 
 sed -ne 's/^\( *\)Subject:/\1/p;/X509v3 Subject Alternative Name/{
-    N;s/^.*\n//;:;s/^\( *\)\(.*\), /\1\2\n\1/;t;p;q; }' < <(
+    N;s/^.*\n//;:a;s/^\( *\)\(.*\), /\1\2\n\1/;t;p;q; }' < <(
     openssl x509 -noout -text -in <(
         openssl s_client -ign_eof 2>/dev/null <<<$'HEAD / HTTP/1.0\r\n\r' \
             -connect $hostname:$port ) )
