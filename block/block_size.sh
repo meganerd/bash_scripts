e7fea63460210fc1de634e7af6de597a60a38fc8
Gustin Johnson <gustin@meganerd.ca>
2018-08-29 09:46:41 -0600

    Added regex to detect vdN, sdN, and hdN block devices to block_size.sh

--- block/block_size.sh
+++ block/block_size.sh
@@ -2,4 +2,4 @@
 
 echo "Iterate through all the attached block devices, listing their physical and logical block sizes."
 
-for each in /sys/block/sd? ; do echo $each ; echo physical ; cat $each/queue/physical_block_size ; echo logical ;cat $each/queue/logical_block_size ; echo "" ;done
+for each in /sys/block/[hsv]d? ; do echo $each ; echo physical ; cat $each/queue/physical_block_size ; echo logical ;cat $each/queue/logical_block_size ; echo "" ;done
