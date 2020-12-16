f745c30295cff27ceebe2c896827be5f436ee99c
Gustin Johnson <gustin@meganerd.ca>
2020-12-16 07:41:12 -0700

    Removed auto sudo use detection.

--- sys/list-swap.sh
+++ sys/list-swap.sh
@@ -3,10 +3,4 @@
 # 
 # This script lists and sorts by size the processes with data currently in swap
 
-# This section tests to see if we are running as root, if not, sudo is used automatically
-if [[ $EUID -ne 0 ]]; then
-USE_SUDO="/usr/bin/sudo"
-else USE_SUDO=""
-fi
-
-$USE_SUDO for file in /proc/*/status ; do $USE_SUDO awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done | sort -k 2 -n -r | less
+for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done | sort -k 2 -n -r | less
