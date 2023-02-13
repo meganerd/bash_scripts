5c7b47e1d47925e4a608ba736a437a6b2fad9983
Gustin Johnson <gustin@meganerd.ca>
2023-02-13 14:19:02 -0700

    Added a line to reinstall skipped packages.

--- apt/update-apt.sh
+++ apt/update-apt.sh
@@ -5,4 +5,8 @@ PATH=$PATH:/usr/local/sbin:/usr/sbin:/sbin:/usr/bin
 
 apt update && apt upgrade -y && apt autoremove -y
 
+echo "Reinstalling packages that were skipped."
+
+for each in $(sudo apt list --upgradable |grep -v Listing |cut -f 1 -d "/") ; do sudo apt reinstall -y $each ; done
+
 echo "Completing update at $(date)"
